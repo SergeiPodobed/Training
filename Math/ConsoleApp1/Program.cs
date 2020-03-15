f51f4e314b85994b14661b45575ff9a8b0cd3934
Компик Наташи) < )@DESKTOP-KEF0OEP>
2020-03-15 19:16:30 +0300

    Created a new project

---
+++ Math/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 10.0;
+            double z = x % 4.0;
+            
+            
+            Console.WriteLine(z);
+        }
+    }
+}
